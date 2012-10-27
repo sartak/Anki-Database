826d65138fc1a60067bd082e80d0361fe6d3638f
Shawn M Moore <code@sartak.org>
2012-10-27 16:20:20 -0400

    WithFields role

---
+++ lib/Anki/Database/WithFields.pm
@@ -0,0 +1,30 @@
+package Anki::Database::WithFields;
+use utf8::all;
+use Any::Moose 'Role';
+
+has fields => (
+    is       => 'ro',
+    isa      => 'HashRef[Maybe[Str]]',
+    required => 1,
+);
+
+around BUILDARGS => sub {
+    my $orig = shift;
+    my $args = $orig->(@_);
+
+    if (!ref($args->{fields})) {
+        my %fields;
+
+        my @keys = @{ $args->{model}->fields };
+        my @values = split "\x1f", $args->{fields};
+
+        @fields{@keys} = @values;
+
+        $args->{fields} = \%fields;
+    }
+
+    return $args;
+};
+
+1;
+
